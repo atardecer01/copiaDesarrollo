70aaa04781312579e24bb08ea188124d70f6bb6b
atardecer01 <rivas.lina@correounivalle.edu.co>
2023-05-17 09:26:04 -0600

    para avanzar

---
+++ frontend/__tests__/components.test.js
@@ -0,0 +1,248 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import Button from '../src/app/components/button.jsx';
+import ButtonBack from '../src/app/components/buttonBack';
+import ExitButton from '../src/app/components/exitButton';
+//import Modal from '../components/registerModal';
+import '@testing-library/jest-dom';
+import axios from 'axios';
+import swal from 'sweetalert';
+
+
+test('Button component renders without crashing', () => {
+  render(<Button texto="Click me" />);
+  const buttonElement = screen.getByRole('button');
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('Button component renders the correct text', () => {
+  const text = 'Click me';
+  render(<Button texto={text} />);
+  const buttonElement = screen.getByRole('button');
+  expect(buttonElement).toHaveTextContent(text);
+});
+
+test('Button component calls the onClick function when clicked', () => {
+  const onClick = jest.fn();
+  render(<Button texto="Click me" onClick={onClick} />);
+  const buttonElement = screen.getByRole('button');
+  fireEvent.click(buttonElement);
+  expect(onClick).toHaveBeenCalledTimes(1);
+});
+
+
+
+
+//------------------------ButtonBack-----------------------------
+test('ButtonBack component renders the correct text', () => {
+    const text = 'Go Back';
+    const onClick = jest.fn(); // Create a mock function for the onClick event
+  
+    render(<ButtonBack texto={text} onClick={onClick} />);
+    const buttonElement = screen.getByRole('button');
+  
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement.textContent).toBe(text);
+  });
+  
+  test('ButtonBack component triggers onClick event', () => {
+    const text = 'Go Back';
+    const onClick = jest.fn();
+  
+    render(<ButtonBack texto={text} onClick={onClick} />);
+    const buttonElement = screen.getByRole('button');
+  
+    fireEvent.click(buttonElement);
+  
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+
+
+  //-----------------ExitButton-------------------
+
+  describe('ExitButton', () => {
+    test('renders button correctly', () => {
+      const onClickMock = jest.fn();
+      const { getByRole } = render(<ExitButton onClick={onClickMock} />);
+      const buttonElement = getByRole('button');
+  
+      expect(buttonElement).toBeInTheDocument();
+      expect(buttonElement).toHaveClass('bg-suspend-session-button-color');
+      expect(buttonElement).toHaveClass('text-white');
+      // ...
+    });
+  
+    test('calls onClick callback when clicked', () => {
+      const onClickMock = jest.fn();
+      const { getByRole } = render(<ExitButton onClick={onClickMock} />);
+      const buttonElement = getByRole('button');
+  
+      fireEvent.click(buttonElement);
+  
+      expect(onClickMock).toHaveBeenCalled();
+    });
+  });
+  
+
+  //--------------------Modal-------------------
+
+ //describe('Modal', () => {
+    /*test('renders register modal correctly', () => {
+      const { queryAllByText, getByRole } = render(<Modal />);
+      expect(queryAllByText('Registrarse')).toHaveLength(0);
+  
+      fireEvent.click(getByRole('link', { name: 'Registrate' }));
+  
+      expect(queryAllByText('Registrarse')).toHaveLength(1);
+
+      const modalTitle = getByText('Registrarse');
+      const nombreInput = getByRole('textbox', { name: 'Nombre' });
+      const emailInput = getByRole('textbox', { name: 'Correo electrónico' });
+      const passwordInput = getByRole('textbox', { name: 'Contraseña' });
+      const confirmPasswordInput = getByRole('textbox', { name: 'Confirmar Contraseña' });
+      const registerButton = getByRole('button', { name: 'Registrarse' });
+  
+      expect(modalTitle).toBeInTheDocument();
+      expect(nombreInput).toBeInTheDocument();
+      expect(emailInput).toBeInTheDocument();
+      expect(passwordInput).toBeInTheDocument();
+      expect(confirmPasswordInput).toBeInTheDocument();
+      expect(registerButton).toBeInTheDocument();
+    });
+  */
+  /*   test('shows error message when form fields are empty', () => {
+      const { getByText, getByRole } = render(<Modal />);
+      const registerLink = getByText('Registrate');
+  
+      fireEvent.click(registerLink);
+  
+      const registerButton = getByRole('button', { name: 'Registrarse' });
+  
+      fireEvent.click(registerButton);
+  
+      const errorMessage = getByText('Por favor llene todos los campos');
+      expect(errorMessage).toBeInTheDocument();
+    });
+
+    test('opens and closes the modal', () => {
+        const { getByText, queryByText } = render(<Modal />);
+      
+        // Verifica que el modal esté cerrado inicialmente
+        expect(queryByText('Registrarse')).toBeNull();
+      
+        // Haz clic en el enlace "Registrate" para abrir el modal
+        fireEvent.click(getByText('Registrate'));
+      
+        // Verifica que el modal esté abierto
+        expect(getByText('Registrarse')).toBeInTheDocument();
+      
+        // Haz clic en el botón de cierre para cerrar el modal
+        fireEvent.click(getByRole('button', { name: 'Cerrar' }));
+      
+        // Verifica que el modal esté cerrado nuevamente
+        expect(queryByText('Registrarse')).toBeNull();
+      });
+
+      test('submits the registration form', () => {
+        const { getByText, getByRole, getByLabelText } = render(<Modal />);
+      
+        // Haz clic en el enlace "Registrate" para abrir el modal
+        fireEvent.click(getByText('Registrate'));
+      
+        // Completa los campos del formulario
+        const nombreInput = getByLabelText('Nombre');
+        const emailInput = getByLabelText('Correo electrónico');
+        const passwordInput = getByLabelText('Contraseña');
+        const confirmPasswordInput = getByLabelText('Confirmar Contraseña');
+        fireEvent.change(nombreInput, { target: { value: 'John Doe' } });
+        fireEvent.change(emailInput, { target: { value: 'john@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'password123' } });
+        fireEvent.change(confirmPasswordInput, { target: { value: 'password123' } });
+      
+        // Haz clic en el botón de registro para enviar el formulario
+        fireEvent.click(getByRole('button', { name: 'Registrarse' }));
+      
+        // Realiza las comprobaciones necesarias para verificar el éxito del registro
+        // por ejemplo, puedes verificar si aparece un mensaje de éxito en la pantalla
+        expect(getByText('¡Registro exitoso!')).toBeInTheDocument();
+      });
+      
+
+    test('verifies password input in Modal component', () => {
+        const { getByRole } = render(<Modal />);
+      
+        // Busca el elemento de contraseña por el atributo role y nombre
+        const passwordInput = getByRole('textbox', { name: 'Contraseña' });
+      
+        // Verifica que se encuentre el elemento de contraseña correctamente
+        expect(passwordInput).toBeInTheDocument();
+      });
+
+    test('registers successfully', async () => {
+        const { getByText, getByRole } = render(<Modal />);
+        const registerLink = getByText('Registrate');
+      
+        fireEvent.click(registerLink);
+      
+        const nombreInput = getByRole('textbox', { name: 'Nombre' });
+        const emailInput = getByRole('textbox', { name: 'Correo electrónico' });
+        const passwordInput = getByRole('textbox', { name: 'Contraseña' });
+        const confirmPasswordInput = getByRole('textbox', { name: 'Confirmar Contraseña' });
+        const registerButton = getByRole('button', { name: 'Registrarse' });
+        fireEvent.change(nombreInput, { target: { value: 'John Doe' } });
+        fireEvent.change(emailInput, { target: { value: 'john@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'password123' } });
+        fireEvent.change(confirmPasswordInput, { target: { value: 'password123' } });
+       
+        axios.post.mockResolvedValue({ data: { msg: 'Registro exitoso' } });
+       
+        fireEvent.click(registerButton);
+       
+        await waitFor(() => {
+          expect(swal).toHaveBeenCalledWith({
+            title: 'Registro exitoso',
+            icon: 'success',
+          });
+        });
+    });
+
+    test('shows error message when registration fails', async () => {
+    const { getByText, getByRole } = render(<Modal />);
+    const registerLink = getByText('Registrate');
+    fireEvent.click(registerLink);
+
+    
+
+    const nombreInput = getByRole('textbox', { name: 'Nombre' });
+    const emailInput = getByRole('textbox', { name: 'Correo electrónico' });
+    const passwordInput = getByLabelText('Contraseña');
+    const confirmPasswordInput = getByRole('textbox', { name: 'Confirmar Contraseña' });
+    const registerButton = getByRole('button', { name: 'Registrarse' });
+   
+    fireEvent.change(nombreInput, { target: { value: 'John Doe' } });
+    fireEvent.change(emailInput, { target: { value: 'john@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    fireEvent.change(confirmPasswordInput, { target: { value: 'password123' } });
+   
+    axios.post.mockRejectedValue({ response: { data: { msg: 'Error' } } });
+   
+    fireEvent.click(registerButton);
+   
+    await waitFor(() => {
+      expect(swal).toHaveBeenCalledWith({
+        title: 'Error',
+        icon: 'warning',
+        button: 'Aceptar',
+      });
+    });
+  });
+      
+  
+    // ...
+  });*/
+  
