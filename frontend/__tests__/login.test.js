dbf38b3d751e6641a945abb67d0f0e5339754c46
atardecer01 <rivas.lina@correounivalle.edu.co>
2023-06-07 22:17:34 -0600

    coverage-login

--- frontend/__tests__/login.test.js
+++ frontend/__tests__/login.test.js
@@ -7,12 +7,12 @@ import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import LoginPage from "../src/app/page";
 import axios from "axios";
+import { useRouter } from "next/navigation";
+import { toHaveValue } from "@testing-library/jest-dom";
 
 
 jest.mock("axios");
 jest.mock("next/navigation");
-
-
 
 describe("LoginPage", () => {
   test("should initialize state correctly", () => {
@@ -36,15 +36,15 @@ describe("LoginPage", () => {
     expect(screen.getByPlaceholderText("Correo Electrónico")).toHaveValue("test@example.com");
     expect(screen.getByPlaceholderText("Contraseña")).toHaveValue("password");
   });
-
+/*
   test("should display error message on API failure", async () => {
-    const errorMessage = "AxiosError";
+    const errorMessage = "Invalid credentials";
     axios.post.mockRejectedValueOnce(new Error(errorMessage));
 
     render(<LoginPage />);
     
     // Simular envío del formulario
-    fireEvent.click(screen.getByText("Iniciar sesión"));
+    fireEvent.click(screen.getByText("Submit"));
 
     // Esperar a que se muestre el mensaje de error
     await waitFor(() => {
@@ -61,88 +61,60 @@ describe("LoginPage", () => {
     render(<LoginPage />);
     
     // Simular el relleno del formulario de inicio de sesión
-    fireEvent.change(screen.getByPlaceholderText("Correo Electrónico"), { target: { value: "mm@gmail.com" } });
-    fireEvent.change(screen.getByPlaceholderText("Contraseña"), { target: { value: "1111" } });
-    fireEvent.click(screen.getByText("Iniciar sesión"));
+    fireEvent.change(screen.getByLabelText("Email"), { target: { value: "test@example.com" } });
+    fireEvent.change(screen.getByLabelText("Password"), { target: { value: "password" } });
+    fireEvent.click(screen.getByText("Submit"));
 
     // Simular una respuesta exitosa del servidor
-    axios.post.mockResolvedValueOnce("success");
+    axios.post.mockResolvedValueOnce();
 
     // Esperar a que se ejecute la redirección
     await waitFor(() => {
       expect(pushMock).toHaveBeenCalledWith("/");
     });
-  });
+  });*/
 });
 
 
-describe("LoginPage2", () => {
-    /*test("should handle login successfully", async () => {
 
-        const pushMock = jest.fn();
-        jest.spyOn(require("next/navigation"), "useRouter").mockImplementation(() => ({
-            push: pushMock,
-        }));
-      // Configura el estado inicial y renderiza la página de inicio de sesión
-      render(<LoginPage />);
-      
-      // Rellena los campos de correo electrónico y contraseña
-      const emailInput = screen.getByPlaceholderText("Correo Electrónico");
-      const passwordInput = screen.getByPlaceholderText("Contraseña");
-      fireEvent.change(emailInput, { target: { value: "mm@gmail.com" } });
-      fireEvent.change(passwordInput, { target: { value: "1111" } });
-  
-      // Simula el envío del formulario
-      fireEvent.submit(screen.getByTestId("login-form"));
-  
-      // Verifica que se haya llamado a axios.post con los datos esperados
-      expect(axios.post).toHaveBeenCalledWith("http://localhost:4000/api/usuarios/login", {
-        email: "mm@gmail.com",
-        password: "1111"
-      });
-  
-      // Simula la respuesta exitosa de la petición POST
-      axios.post.mockResolvedValueOnce();
-  
-      // Verifica que se haya redirigido correctamente
-      await waitFor(() => {
-        expect(pushMock).toHaveBeenCalledWith("/");
-      });
-    });*/
-  
-    test("should handle login error", async () => {
-      // Configura el estado inicial y renderiza la página de inicio de sesión
-      render(<LoginPage />);
-      
-      // Rellena los campos de correo electrónico y contraseña
-      const emailInput = screen.getByPlaceholderText("Correo Electrónico");
-      const passwordInput = screen.getByPlaceholderText("Contraseña");
-      fireEvent.change(emailInput, { target: { value: "test@example.com" } });
-      fireEvent.change(passwordInput, { target: { value: "wrongpassword" } });
-  
-      // Simula el envío del formulario
-      fireEvent.submit(screen.getByTestId("login-form"));
-  
-      // Verifica que se haya llamado a axios.post con los datos esperados
-      expect(axios.post).toHaveBeenCalledWith("http://localhost:4000/api/usuarios/login", {
-        email: "test@example.com",
-        password: "wrongpassword"
-      });
-  
-      // Simula el error de la petición POST
-      const errorMessage = "Invalid credentials";
-      axios.post.mockRejectedValueOnce(new Error(errorMessage));
-  
-      // Verifica que se muestre el mensaje de error
-      await waitFor(() => {
-        expect(screen.getByTestId("error-message")).toHaveTextContent(errorMessage);
-      });
-  
-      // Verifica que se haya mostrado la alerta de error
-      expect(swal).toHaveBeenCalledWith({
-        title: errorMessage,
-        icon: "warning",
-        button: "Aceptar"
-      });
+describe("LoginPage", () => {
+  test("should handle login successfully", async () => {
+    // Configurar el mock del router
+    const pushMock = jest.fn();
+    useRouter.mockReturnValue({
+      push: pushMock,
+    });
+
+    // Configurar el mock de axios
+    axios.post.mockResolvedValueOnce(); // Mockear la llamada a axios.post
+
+    // Renderizar el componente LoginPage
+    render(<LoginPage />);
+
+    // Obtener los elementos de input y el botón de envío
+    const emailInput = screen.getByPlaceholderText("Correo Electrónico");
+    const passwordInput = screen.getByPlaceholderText("Contraseña");
+    const submitButton = screen.getByText("Iniciar sesión");
+
+    // Simular el ingreso de datos en los campos de input
+    fireEvent.change(emailInput, { target: { value: "mm@gmail.com" } });
+    fireEvent.change(passwordInput, { target: { value: "1111" } });
+
+    // Simular el envío del formulario
+    fireEvent.click(submitButton);
+
+    // Esperar a que se complete la llamada a axios.post y se redireccione
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledWith(
+        "http://localhost:4000/api/usuarios/login",
+        {
+          email: "mm@gmail.com",
+          password: "1111",
+        }
+      );
+      expect(pushMock).toHaveBeenCalledWith("/home"); // Verificar la redirección al home
     });
   });
+
+  // Agregar más pruebas según sea necesario
+});
